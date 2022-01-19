b1d714ba4f57f5a3e6e0f5a6db08a3bc5648d554
Reid <e079004@gmail.com>
2022-01-19 20:27:58 +0800

    add schema

--- upload_data.py
+++ upload_data.py
@@ -1,70 +1,66 @@
+from google.cloud import bigquery as bq
+
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "bigquery_key.json"
+
 from datetime import datetime
 today = datetime.today().strftime("%Y%m%d")
 
 query = (
     '''
-        MERGE
-        china-airlines-338006.JourneyMessage.JourneyMessageHistory_Others T
-        USING
-        china-airlines-338006.JourneyMessage.JourneyMessageHistory_Others_{name} S
-        ON
-        T.system_id = S.system_id
-        WHEN NOT MATCHED THEN INSERT (`app_fail`, `app_open`, `app_success`, `arrival_station__c`, `birthday_event_date`, `card_no__c`, `card_type__c`, `content_name__c`, `date`, `delivered_count`, `departure_station__c`, `edm_bounce`, `edm_click`, `edm_open`, `edm_sent`, `iid`, `journey_content__r_a_b_test__c`, `nfp_card_id_check`, `nfp_delivered`, `nfp_sends`, `pnr_number`, `send_count`, `sent_date__c`, `sms_delivered`, `sms_undelivered`, `status__c`, `system_id`, `trn_card_id_check`, `trn_delivered`, `trn_sends`, `type__c`, `utm_content__c`) VALUES (`app_fail`, `app_open`, `app_success`, `arrival_station__c`, `birthday_event_date`, `card_no__c`, `card_type__c`, `content_name__c`, `date`, `delivered_count`, `departure_station__c`, `edm_bounce`, `edm_click`, `edm_open`, `edm_sent`, `iid`, `journey_content__r_a_b_test__c`, `nfp_card_id_check`, `nfp_delivered`, `nfp_sends`, `pnr_number`, `send_count`, `sent_date__c`, `sms_delivered`, `sms_undelivered`, `status__c`, `system_id`, `trn_card_id_check`, `trn_delivered`, `trn_sends`, `type__c`, `utm_content__c`)
-        WHEN MATCHED
-        THEN
-        UPDATE
-        SET
-        app_fail = S.app_fail,
-        app_open = S.app_open,
-        app_success = S.app_success,
-        arrival_station__c = S.arrival_station__c,
-        birthday_event_date = S.birthday_event_date,
-        card_no__c = S.card_no__c,
-        card_type__c = S.card_type__c,
-        content_name__c = S.content_name__c,
-        date = S.date,
-        delivered_count = S.delivered_count,
-        departure_station__c = S.departure_station__c,
-        edm_bounce = S.edm_bounce,
-        edm_click = S.edm_click,
-        edm_open = S.edm_open,
-        edm_sent = S.edm_sent,
-        iid = S.iid,
-        journey_content__r_a_b_test__c = S.journey_content__r_a_b_test__c,
-        nfp_card_id_check = S.nfp_card_id_check,
-        nfp_delivered = S.nfp_delivered,
-        nfp_sends = S.nfp_sends,
-        pnr_number = S.pnr_number,
-        send_count = S.send_count,
-        sent_date__c = S.sent_date__c,
-        sms_delivered = S.sms_delivered,
-        sms_undelivered = S.sms_undelivered,
-        status__c = S.status__c,
-        system_id = S.system_id,
-        trn_card_id_check = S.trn_card_id_check,
-        trn_delivered = S.trn_delivered,
-        trn_sends = S.trn_sends,
-        type__c = S.type__c,
-        utm_content__c = S.utm_content__c;
+        CREATE TABLE `china-airlines-338006.JourneyMessage.JourneyMessageHistory_Others_{name}` S
     '''
     .format(name=today)
 )
 
 
 def upload_data(query=query, project_id="driven-stage-300605"):
-    print(__name__)
-    import os
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "bigquery_key.json"
-    from google.cloud import bigquery as bq
     client = bq.Client()
-    print("upload data")
     query_job = client.query(query)
     rows = query_job.result()
     for row in rows:
         print(row)
 
 def read_csv_file(csv_file_path="D:\\Data\\SFMC\\JourneyMessageHistory_Others.csv"):
-    import pandas as pd
 
+    import pandas as pd
+    schema = [
+        bq.SchemaField('system_id STRING'),
+        bq.SchemaField('sent_date__c STRING'),
+        bq.SchemaField('content_name__c STRING'),
+        bq.SchemaField('journey_content__r_a_b_test__c STRING'),
+        bq.SchemaField('type__c STRING'),
+        bq.SchemaField('card_no__c STRING'),
+        bq.SchemaField('card_type__c STRING'),
+        bq.SchemaField('status__c STRING'),
+        bq.SchemaField('arrival_station__c STRING'),
+        bq.SchemaField('departure_station__c STRING'),
+        bq.SchemaField('pnr_number STRING'),
+        bq.SchemaField('utm_content__c STRING'),
+        bq.SchemaField('birthday_event_date STRING'),
+        bq.SchemaField('iid STRING'),
+        bq.SchemaField('date date'),
+        bq.SchemaField('edm_sent INT'),
+        bq.SchemaField('edm_bounce INT'),
+        bq.SchemaField('edm_open INT'),
+        bq.SchemaField('edm_click INT'),
+        bq.SchemaField('app_open INT'),
+        bq.SchemaField('app_success INT'),
+        bq.SchemaField('app_fail INT'),
+        bq.SchemaField('sms_delivered INT'),
+        bq.SchemaField('sms_undelivered INT'),
+        bq.SchemaField('trn_delivered INT'),
+        bq.SchemaField('trn_sends INT'),
+        bq.SchemaField('delivered_count INT'),
+        bq.SchemaField('send_count INT'),
+        bq.SchemaField('nfp_delivered INT'),
+        bq.SchemaField('nfp_sends INT'),
+        bq.SchemaField('trn_card_id_check STRING'),
+        bq.SchemaField('nfp_card_id_check STRING')
+    ]
+
+    pd.read_csv(csv_file_path).to_sql
+
+    
 
 upload_data()
