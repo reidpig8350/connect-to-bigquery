b6e8275028fbfb5f2e84822c87682b02eab55d0d
Reid <e079004@gmail.com>
2022-01-21 16:45:45 +0800

    cloud storage

--- upload_data.py
+++ upload_data.py
@@ -8,7 +8,40 @@ today = datetime.today().strftime("%Y%m%d")
 
 query = (
     '''
-        CREATE TABLE `china-airlines-338006.JourneyMessage.JourneyMessageHistory_Others_{name}` S
+        CREATE TABLE `china-airlines-338006.JourneyMessage.JourneyMessageHistory_Others_{name}` (
+            system_id STRING,
+            sent_date__c STRING,
+            content_name__c STRING,
+            journey_content__r_a_b_test__c STRING,
+            type__c STRING,
+            card_no__c STRING,
+            card_type__c STRING,
+            status__c STRING,
+            arrival_station__c STRING,
+            departure_station__c STRING,
+            pnr_number STRING,
+            utm_content__c STRING,
+            birthday_event_date STRING,
+            iid STRING,
+            date date,
+            edm_sent INT,
+            edm_bounce INT,
+            edm_open INT,
+            edm_click INT,
+            app_open INT,
+            app_success INT,
+            app_fail INT,
+            sms_delivered INT,
+            sms_undelivered INT,
+            trn_delivered INT,
+            trn_sends INT,
+            delivered_count INT,
+            send_count INT,
+            nfp_delivered INT,
+            nfp_sends INT,
+            trn_card_id_check STRING,
+            nfp_card_id_check STRING
+        );
     '''
     .format(name=today)
 )
@@ -16,51 +49,41 @@ query = (
 
 def upload_data(query=query, project_id="driven-stage-300605"):
     client = bq.Client()
-    query_job = client.query(query)
-    rows = query_job.result()
-    for row in rows:
-        print(row)
+    # query_job = client.query(query)
+    # rows = query_job.result()
+    # for row in rows:
+    #     print(row)
 
-def read_csv_file(csv_file_path="D:\\Data\\SFMC\\JourneyMessageHistory_Others.csv"):
+    job_config = bq.LoadJobConfig(
+        schema=[
+            bq.SchemaField('system_id', 'STRING'),
+            bq.SchemaField('sent_date__c', 'STRING'),
+            bq.SchemaField('content_name__c', 'STRING'),
+            bq.SchemaField('journey_content__r_a_b_test__c', 'STRING'),
+            bq.SchemaField('type__c', 'STRING'),
+            bq.SchemaField('card_no__c', 'STRING'),
+            bq.SchemaField('card_type__c', 'STRING'),
+            bq.SchemaField('status__c', 'STRING'),
+            bq.SchemaField('arrival_station__c', 'STRING'),
+            bq.SchemaField('departure_station__c', 'STRING'),
+            bq.SchemaField('pnr_number', 'STRING'),
+            bq.SchemaField('utm_content__c', 'STRING'),
+            bq.SchemaField('birthday_event_date', 'STRING')
+        ],  
+        skip_leading_rows=1,
+        # The source format defaults to CSV, so the line below is optional.
+        source_format=bq.SourceFormat.CSV,
+    )
 
-    import pandas as pd
-    schema = [
-        bq.SchemaField('system_id STRING'),
-        bq.SchemaField('sent_date__c STRING'),
-        bq.SchemaField('content_name__c STRING'),
-        bq.SchemaField('journey_content__r_a_b_test__c STRING'),
-        bq.SchemaField('type__c STRING'),
-        bq.SchemaField('card_no__c STRING'),
-        bq.SchemaField('card_type__c STRING'),
-        bq.SchemaField('status__c STRING'),
-        bq.SchemaField('arrival_station__c STRING'),
-        bq.SchemaField('departure_station__c STRING'),
-        bq.SchemaField('pnr_number STRING'),
-        bq.SchemaField('utm_content__c STRING'),
-        bq.SchemaField('birthday_event_date STRING'),
-        bq.SchemaField('iid STRING'),
-        bq.SchemaField('date date'),
-        bq.SchemaField('edm_sent INT'),
-        bq.SchemaField('edm_bounce INT'),
-        bq.SchemaField('edm_open INT'),
-        bq.SchemaField('edm_click INT'),
-        bq.SchemaField('app_open INT'),
-        bq.SchemaField('app_success INT'),
-        bq.SchemaField('app_fail INT'),
-        bq.SchemaField('sms_delivered INT'),
-        bq.SchemaField('sms_undelivered INT'),
-        bq.SchemaField('trn_delivered INT'),
-        bq.SchemaField('trn_sends INT'),
-        bq.SchemaField('delivered_count INT'),
-        bq.SchemaField('send_count INT'),
-        bq.SchemaField('nfp_delivered INT'),
-        bq.SchemaField('nfp_sends INT'),
-        bq.SchemaField('trn_card_id_check STRING'),
-        bq.SchemaField('nfp_card_id_check STRING')
-    ]
+    uri = "gs://journey_message/1JourneyMessageHistory_Others - 工作表1 (1).csv"
 
-    pd.read_csv(csv_file_path).to_sql
+    table_id = 'china-airlines-338006.JourneyMessage.JourneyMessageHistory_Others_{name}' .format(name = today)
+    load_job = client.load_table_from_uri(
+        uri, table_id, job_config=job_config
+    )  # Make an API request.
 
-    
+    load_job.result()  # Waits for the job to complete.
+    destination_table = client.get_table(table_id)  # Make an API request.
+    print("Loaded {} rows." .format(destination_table.num_rows))
 
 upload_data()
